bb8e8daf364faa8bbd8936a7c650b06a099e71cf
MrZMaxV <152068432+MrZMaxV@users.noreply.github.com>
2024-04-10 10:47:55 +0200

    garbage removal

--- StringManipulation1/src/ua/com/foxminded/stringmanipulation/StringUtil.java
+++ StringManipulation1/src/ua/com/foxminded/stringmanipulation/StringUtil.java
@@ -1,9 +1,5 @@
 package ua.com.foxminded.stringmanipulation;
 
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-
 public class StringUtil {
 
 	public static void main(String[] args) {
